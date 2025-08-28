1ae6d98905d94e1171baa9c0af3907a8b6bbf768
Erik Smith <eriksmith.career@hotmail.com>
2025-08-28 17:05:36 +0100

    demo code

--- chapter1.py
+++ chapter1.py
@@ -1,6 +1,20 @@
+import random
+
 customers = ['Jimmy', 'John', 'Stacie']
 
 winner = random.choice(customers)
 
 flavor = 'vanilla'
 
+print('Congratulations ' + winner + + ' you have won an ice cream sundae.')
+
+prompt = ' Would you like a cherry on top? '
+ 
+wants_cherry = input(prompt)
+
+order = flavor + 'sundae'
+
+if (wants_cherry == 'yes'):
+    order = order + ' with cherry on top'
+
+print('One ' + order + ' for ' + winner  + ' coming right up... ')
