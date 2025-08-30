ec098365108d23f9427e32b3dca00c66e76b86d5
Erik Smith <eriksmith.career@hotmail.com>
2025-08-30 10:18:28 +0100

    ch1 ex1

--- chapter1.py
+++ chapter1.py
@@ -1,20 +1,32 @@
+# import random
+
+# customers = ['Jimmy', 'John', 'Stacie']
+
+# winner = random.choice(customers)
+
+# flavor = 'vanilla '
+
+# print('Congratulations ' + winner + ' you have won an ice cream sundae.')
+
+# prompt = ' Would you like a cherry on top? '
+ 
+# wants_cherry = input(prompt)
+
+# order = flavor + 'sundae'
+
+# if (wants_cherry.lower() == 'yes'):
+#     order = order + ' with cherry on top'
+
+# print('One ' + order + ' for ' + winner  + ' coming right up... ')
+
+#EXERCISE 2
+
+#Let Python know we'll be using somne random functionality by importing the random module.
 import random
 
-customers = ['Jimmy', 'John', 'Stacie']
+#Make three lists one of verbs one of adjectives and one of nouns
+verbs = ['Leverage', 'Sync', 'Target','Gamify', 'Offline', 'Crowd-sourced', '24/7', 'Lean-in', '30,000']
 
-winner = random.choice(customers)
+adjectives = ['A/B Tested', 'Freemium', 'Hyperlocal','Siloed', 'B-to-B', 'Oriented', 'Cloud Based', 'API based']
 
-flavor = 'vanilla '
-
-print('Congratulations ' + winner + ' you have won an ice cream sundae.')
-
-prompt = ' Would you like a cherry on top? '
- 
-wants_cherry = input(prompt)
-
-order = flavor + 'sundae'
-
-if (wants_cherry.lower() == 'yes'):
-    order = order + ' with cherry on top'
-
-print('One ' + order + ' for ' + winner  + ' coming right up... ')
+nouns = ['Early Adopter', 'Low hanging Fruit', 'Pipe-line', 'Splash Page', 'Productivity',]
